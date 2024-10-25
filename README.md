ba5089b47375e4320d4e3df4ba3e123650b5fdd5
jayel888 <15644@coderacademy.edu.au>
2024-10-26 10:18:58 +1100

    feat: Initialise our node environment and created a simple function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # t3w2-js-test
+# Learning testing with JEST (JS Testing Framework)
+# TDD Concepts
+# Building a basic terminal app with TDD
+
+Terminal app idea:
+- Log a message
+    - a message that repeats itself.
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { niceLogger } = require("./niceLogger");
+niceLogger
+// Import the file
+// const niceLoggerFile = require("./niceLogger.js");
+
+// Import a specific function from the file
+// const niceLoggerFunction = niceLoggerFile.niceLogger;
